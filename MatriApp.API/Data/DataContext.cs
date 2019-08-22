375cf280eb19cb10114d5ad4083043071393659a
nivethini <uvengat@gmail.com>
2019-08-22 22:06:55 +0530

    first commit

---
+++ MatriApp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using MatriApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MatriApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DbSet<Value> Values { get; set;}
+
+    }
+    
+}
