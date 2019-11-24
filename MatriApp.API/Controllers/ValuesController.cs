5cc8f18fc2a0744c0e0e64d2962a3f3c24324070
nivethini <uvengat@gmail.com>
2019-11-24 21:40:14 +0530

    Authendicatieon added

--- MatriApp.API/Controllers/ValuesController.cs
+++ MatriApp.API/Controllers/ValuesController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MatriApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace MatriApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -27,6 +29,7 @@ namespace MatriApp.API.Controllers
             return Ok(values);
         }
 
+        [AllowAnonymous]
         [HttpGet("{id}")]
            public async Task<IActionResult> GetValue(int id)
         {
